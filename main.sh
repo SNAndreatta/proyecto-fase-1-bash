2a637825011be91c4b9b8315c3baa4bba5840c65
Santino Andreatta <santinoandreatta@gmail.com>
2026-08-29 23:00:22 -0300

    Update main.sh

--- main.sh
+++ main.sh
@@ -5,10 +5,9 @@ if [ -z "$FILENAME" ]; then
 fi
 
 if [ "$1" == "-d" ]; then
-    echo "Destruyendo entorno y deteniendo procesos..."
+    echo "Destruyendo entorno y deteniendo procesos."
     pkill -f "consolidar.sh" 2>/dev/null
     rm -rf "$HOME/EPNro1/"
-    echo "Entorno eliminado exitosamente."
     exit 0
 fi
 
@@ -23,10 +22,10 @@ while [ "$opcion" != 7 ]; do
     echo "5) Buscar datos por número de padrón"
     echo "6) Visualizar log"
     echo "7) Salir"
-    read -p "Selecciona una opción: " opcion
+    read -p "Seleccioná una opción: " opcion
 
     case $opcion in
-        # Crear entorno
+        # Crear entorno.
         1)
             clear
             mkdir "$HOME/EPNro1/" "$HOME/EPNro1/entrada" "$HOME/EPNro1/salida" "$HOME/EPNro1/procesado"
@@ -38,30 +37,30 @@ while [ "$opcion" != 7 ]; do
                 echo "Error: No se encontró 'consolidar.sh' en el directorio actual."
             fi
             ;;
-        # Iniciar el proceso en background
+        # Iniciar el proceso en background.
         2)
             clear
             if [ -f "$HOME/EPNro1/consolidar.sh" ]; then
-                echo "Iniciando proceso en background..."
+                echo "Iniciando proceso en background."
                 "$HOME/EPNro1/consolidar.sh" &
             else
-                echo "No existe el script. Tenes que crear el entorno"
+                echo "No existe el script. Tenés que crear el entorno"
             fi
             ;;
-        # Mostrar listado ordenado por padrón
+        # Mostrar listado ordenado por padrón.
         3)
             clear
             if [ -d "$HOME/EPNro1" ]; then
                 if [ -f "$HOME/EPNro1/salida/$FILENAME.txt" ]; then
                     sort -n -k1 "$HOME/EPNro1/salida/$FILENAME.txt"
                 else 
-                    echo "El archivo no existe"
+                    echo "El archivo no existe."
                 fi
             else
                 echo "No está creado el entorno. Tenés que crearlo."
             fi
             ;;
-        # Mostrar las 10 notas más altas
+        # Mostrar las 10 notas más altas.
         4)
             clear
             if [ -d "$HOME/EPNro1" ]; then
@@ -70,39 +69,38 @@ while [ "$opcion" != 7 ]; do
                     sort -k5 -n -r "$HOME/EPNro1/salida/$FILENAME.txt" | head -n 10
 
                 else
-                    echo "El archivo no existe"
+                    echo "El archivo no existe."
 
                 fi
             else
                 echo "No está creado el entorno. Tenés que crearlo."
             fi
             ;;
-        # Buscar datos por número de padrón
+        # Buscar datos por número de padrón.
         5)
             clear
             if [ -d "$HOME/EPNro1" ]; then
                 archivo="$HOME/EPNro1/salida/$FILENAME.txt"
 
                 if [ -f "$archivo" ]; then
-                    read -p "Ingrese el numero del padron: " padron
+                    read -p "Ingresá el número del padrón: " padron
                     
                     resultado=$(grep "^$padron " "$archivo")
 
                     if [ -n "$resultado" ]; then
                         echo "$resultado"
                     else
-                        echo "No se encontro el padron"
-
+                        echo "No se encontró el padrón."
                     fi        
 
                 else
-                    echo "El archivo no existe"
+                    echo "El archivo no existe."
                 fi
             else
                 echo "No está creado el entorno. Tenés que crearlo."
             fi
             ;;
-        # Visualizar log
+        # Visualizar log.
         6)
             clear
             if [ -d "$HOME/EPNro1" ]; then
@@ -111,21 +109,20 @@ while [ "$opcion" != 7 ]; do
                     echo "Archivo de logs"
                     cat "$HOME/EPNro1/procesado.log"
                 else
-                    echo "El archivo de logs no existe"
+                    echo "El archivo de logs no existe."
                 fi
             else
                 echo "No está creado el entorno. Tenés que crearlo."
             fi
             ;;
-        # Salir del programa¿
+        # Salir del programa.
         7)
             clear
-            echo "Saliendo..."
             ;;
-        # Si la opción ingresada no es un nro del 1 al 7
+        # Si la opción ingresada no es un número del 1 al 7.
         *)
             clear
-            echo "Por favor ingresar un número del 1 al 7"
+            echo "Por favor ingresá un número del 1 al 7."
             ;;
     esac
 done
